b71fd686adf1cadf0c4afade71c6fc758c9ca240
ChamodiDH <chamodi3797@gmail.com>
2022-09-14 14:57:50 +0530

    NavBar implementation

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function NavBar(){
+
+  return(
+
+
+    <div className='nav'>
+        <img class="icon-earth" src="../images/earth.png" alt="earth is displayed here"/>
+        <h1 class="title-navbar">my travel journal</h1>
+    </div>
+
+
+
+
+
+  )
+
+
+}
